c050ae5daa2a58505505c8dd68f8eff35fde64f6
alijuvance <alijuvance@gmail.com>
2026-06-20 18:25:02 +0300

    feat(api): register suppliers and purchase-orders modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -11,9 +11,11 @@ import { DepotsModule } from './depots/depots.module';
 import { StockLevelsModule } from './stock-levels/stock-levels.module';
 import { StockMovementsModule } from './stock-movements/stock-movements.module';
 import { StockTransfersModule } from './stock-transfers/stock-transfers.module';
+import { SuppliersModule } from './suppliers/suppliers.module';
+import { PurchaseOrdersModule } from './purchase-orders/purchase-orders.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, CategoriesModule, ProductsModule, UsersModule, RolesModule, DepotsModule, StockLevelsModule, StockMovementsModule, StockTransfersModule],
+  imports: [PrismaModule, AuthModule, CategoriesModule, ProductsModule, UsersModule, RolesModule, DepotsModule, StockLevelsModule, StockMovementsModule, StockTransfersModule, SuppliersModule, PurchaseOrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
