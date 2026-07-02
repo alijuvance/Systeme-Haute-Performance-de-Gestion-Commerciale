6d8958d8aac89b5234b6fb2e30ccfec8a95eba64
alijuvance <alijuvance@gmail.com>
2026-07-02 19:00:29 +0300

    feat(roles): implement roles module for RBAC

--- backend/src/modules/roles/roles.controller.ts
+++ backend/src/modules/roles/roles.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../../common/decorators/roles.decorator';
+import { Controller, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles(...['ADMIN'])
 @Controller('roles')
 export class RolesController {}
