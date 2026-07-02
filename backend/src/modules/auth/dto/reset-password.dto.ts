868d1c4d36dd15508704ff50ddfafb115d12ca42
alijuvance <alijuvance@gmail.com>
2026-07-02 17:37:16 +0300

    audi pour le TOKEN qui sont deja implementer

---
+++ backend/src/modules/auth/dto/reset-password.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class ResetPasswordDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  otp: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6, { message: 'Le mot de passe doit contenir au moins 6 caractères' })
+  newPassword: string;
+}
